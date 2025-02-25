6c6b21a692550f5cf1eab66634f1909638ce1a0a
Vitor Hugo Sodré Quinelato <vitorhugoquinelato@gmail.com>
2025-02-25 14:41:52 -0400

    arrumando/2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Prova2_WEB1/', // Defina o nome do seu repositório aqui
+  root: 'public', // Define que a pasta raiz será "public"
+  build: {
+    outDir: '../dist'} // Define onde será gerada a build final
 });
